57c0990e576496afe7d29a125e061e43cd8125de
Alisson Wilker <alissonwilker@gmail.com>
2017-07-29 15:21:54 -0300

    Adicionando o log4j logger.

--- lpoow_tomcat/src/main/java/br/edu/ifb/listener/HibernateSessionFactoryListener.java
+++ lpoow_tomcat/src/main/java/br/edu/ifb/listener/HibernateSessionFactoryListener.java
@@ -4,6 +4,7 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
+import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
@@ -14,23 +15,32 @@ import br.edu.ifb.model.Employee;
 @WebListener
 public class HibernateSessionFactoryListener implements ServletContextListener {
 
+	private final Logger logger = Logger.getLogger(HibernateSessionFactoryListener.class);
+	
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
     	SessionFactory sessionFactory = (SessionFactory) servletContextEvent.getServletContext().getAttribute("SessionFactory");
     	if(sessionFactory != null && !sessionFactory.isClosed()){
+    		logger.info("Fechando o sessionFactory");
     		sessionFactory.close();
     	}
+		logger.info("Liberado o recurso Hibernate sessionFactory");
     }
 
     public void contextInitialized(ServletContextEvent servletContextEvent) {
     	Configuration configuration = new Configuration();
     	configuration.configure("hibernate.cfg.xml");
     	configuration.addAnnotatedClass(Employee.class);
+		logger.info("Hibernate Configuration criada com sucesso");
     	
     	ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+    	logger.info("Hibernate ServiceRegistry criado com sucesso");
+    	
     	SessionFactory sessionFactory = configuration
 				.buildSessionFactory(serviceRegistry);
-    	
+    	logger.info("Hibernate SessionFactory criada com sucesso");
     	servletContextEvent.getServletContext().setAttribute("SessionFactory", sessionFactory);
+    	
+    	logger.info("Hibernate SessionFactory configurada com sucesso");
     }
 	
 }
